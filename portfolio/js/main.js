06f3e259062d5636bd6ffeeaa5656ca2d7cb19f6
SiebeVerhaegen <r1002687@student.thomasmore.be>
2023-10-12 20:32:24 +0200

    Aanmaken CSS voor pagina Ervaring. + Toevoegen script aan Home pagina

---
+++ portfolio/js/main.js
@@ -0,0 +1,31 @@
+function hoverCV() {
+    document.getElementById("imgCV").src="afbeeldingen/icoon-cv-inverted.png";
+}
+
+function leaveCV() {
+    document.getElementById("imgCV").src="afbeeldingen/icoon-cv.png";
+}
+
+function hoverErvaring() {
+    document.getElementById("imgErvaring").src="afbeeldingen/icoon-ervaring-inverted.png";
+}
+
+function leaveErvaring() {
+    document.getElementById("imgErvaring").src="afbeeldingen/icoon-ervaring.png";
+}
+
+function hoverHobbys() {
+    document.getElementById("imgHobbys").src="afbeeldingen/icoon-hobbys-inverted.png";
+}
+
+function leaveHobbys() {
+    document.getElementById("imgHobbys").src="afbeeldingen/icoon-hobbys.png";
+}
+
+function hoverContact() {
+    document.getElementById("imgContact").src="afbeeldingen/icoon-contact-inverted.png";
+}
+
+function leaveContact() {
+    document.getElementById("imgContact").src="afbeeldingen/icoon-contact.png";
+}
