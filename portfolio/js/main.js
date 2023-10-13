01197c32759eda5a27ea38dd19a017ce10b08d4d
SiebeVerhaegen <r1002687@student.thomasmore.be>
2023-10-13 21:48:33 +0200

    Aanmaken CSS voor Hobby's pagina.

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,3 +1,6 @@
+const animationSizeInterval = 3;
+const animationPosInetrval = 1.5;
+
 function hoverCV() {
     document.getElementById("imgCV").src="afbeeldingen/icoon-cv-inverted.png";
 }
@@ -28,4 +31,32 @@ function hoverContact() {
 
 function leaveContact() {
     document.getElementById("imgContact").src="afbeeldingen/icoon-contact.png";
+}
+
+function buttonLinkAnimation(element) {
+    let id = null;
+    const elem = document.getElementById(element);
+    let pos = 49;
+    let size = 2;
+    clearInterval(id);
+    elem.hidden = false;
+    id = setInterval(frame, 5);
+    function frame() {
+      if (size >= 100) {
+        elem.hidden = true;
+        clearInterval(id);
+      } else {
+        pos -= animationPosInetrval;
+        size += animationSizeInterval;
+        elem.style.top = pos + "%"; 
+        elem.style.left = pos + "%"; 
+        elem.style.width = size + "%";
+        elem.style.height = size + "%";
+      }
+    }
+}
+
+function stopAnimation(element) {
+    const elem = document.getElementById(element);
+    elem.hidden = true;
 }
