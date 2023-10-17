5ef24b136747ba28a5c4006e519b2a0a87824272
SiebeVerhaegen <r1002687@student.thomasmore.be>
2023-10-17 15:31:31 +0200

    Afwerken contact pagina.

--- portfolio/PHP/studentenmail.php
+++ portfolio/PHP/studentenmail.php
@@ -1,18 +1,16 @@
 <?php
-$studentemail = htmlspecialchars($_POST['to']);
-$subject = htmlspecialchars($_POST['subject']);
-$message= htmlspecialchars($_POST['message']);
-$headers[] = 'MIME-Version: 1.0';
-$headers[] = 'Content-type: text/html; charset=iso-8859-1';
-$headers[] = 'From: Name-here <from@yourdomain.be>';
-$headers[] = 'Cc: ccmail@yourdomain.be';
+$studentemail = htmlspecialchars($_POST['studentemail']);
+$onderwerp = htmlspecialchars($_POST['onderwerp']);
+$inhoud = htmlspecialchars($_POST['inhoud']);
+$headers[] = 'Vraag Portfolio';
+$headers[] = "Naam: " + htmlspecialchars($_POST['naam']);
+$headers[] = "E-mail: " + htmlspecialchars($_POST['email']);
 // using mail function and returns boolean
-$send = mail($studentemail, $subject, $txt, implode("\r\n", $headers));
+$send = mail($studentemail, $onderwerp, $inhoud, implode("\r\n", $headers));
 if ($send) {
     $response = "Mail sent successfully";
 } else {
     $response = "Mail not sent";
 }
-exit($response);
 echo json_encode($response);
 ?>
