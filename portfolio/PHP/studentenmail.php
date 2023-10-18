1175d57a66b6056c0671e9bb7cebae68acd5dd15
SiebeVerhaegen <r1002687@student.thomasmore.be>
2023-10-18 11:07:06 +0200

    Oplossen mail() functie in PHP bestand.

--- portfolio/PHP/studentenmail.php
+++ portfolio/PHP/studentenmail.php
@@ -2,11 +2,15 @@
 $studentemail = htmlspecialchars($_POST['studentemail']);
 $onderwerp = htmlspecialchars($_POST['onderwerp']);
 $inhoud = htmlspecialchars($_POST['inhoud']);
-$headers[] = 'Vraag Portfolio';
-$headers[] = "Naam: " + htmlspecialchars($_POST['naam']);
-$headers[] = "E-mail: " + htmlspecialchars($_POST['email']);
+$naam = htmlspecialchars($_POST['naam']);
+$email = htmlspecialchars($_POST['email']);
+$volledigeInhoud = "Naam: {$naam}\nE-mail: {$email}\n\n{$inhoud}";
+
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/plain; charset=iso-8859-1';
+$headers[] = 'From: Siebe <services@tm-statweb.be>';
 // using mail function and returns boolean
-$send = mail($studentemail, $onderwerp, $inhoud, implode("\r\n", $headers));
+$send = mail($studentemail, $onderwerp, $volledigeInhoud, implode("\r\n", $headers));
 if ($send) {
     $response = "Mail sent successfully";
 } else {
